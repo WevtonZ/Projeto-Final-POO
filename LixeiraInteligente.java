0589c4aa0ee83c1378dae3f2bf8037dd6a97773a
Wevton Junior <wevtonj@gmail.com>
2025-06-30 11:17:42 -0300

    Primeiro de muitos commmmmmit

---
+++ LixeiraInteligente.java
@@ -0,0 +1,6 @@
+public class LixeiraInteligente {
+    private final int id; // identificador da lixeira
+    private final double capLitros; // capacidade da lixeira em flutuante;
+    private final Localizacao loc; // lugar onde a lixeira está.
+    private double qtdAtual; // Quantidade de lixo na lixeira atualmente.
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+// Importar coisas de biblioteca gráfica.
+// Como usá-las, ainda não sei, mas descobrirei no meio do caminho.
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
