c6351339ceaadd46b01de8122bf0ae5e38583d70
Wevton Junior <wevtonj@gmail.com>
2025-06-30 18:20:57 -0300

    Delete LixeiraInteligente.java

--- LixeiraInteligente.java
+++
@@ -1,6 +0,0 @@
-public class LixeiraInteligente {
-    private final int id; // identificador da lixeira
-    private final double capLitros; // capacidade da lixeira em flutuante;
-    private final Localizacao loc; // lugar onde a lixeira está.
-    private double qtdAtual; // Quantidade de lixo na lixeira atualmente.
-}
