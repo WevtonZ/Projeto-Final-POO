3081fdfbcae45d3564f8b3a482cdeba4c41fcd06
Wevton Junior <wevtonj@gmail.com>
2025-07-03 23:20:08 -0300

    Delete src/com/suaempresa/cidadeinteligente/simulador/Main.java

--- src/com/suaempresa/cidadeinteligente/simulador/Main.java
+++
@@ -1,8 +0,0 @@
-package com.suaempresa.cidadeinteligente.simulador;// Importar coisas de biblioteca gráfica.
-// Como usá-las, ainda não sei, mas descobrirei no meio do caminho.
-
-public class Main {
-    public static void main(String[] args){
-
-    }
-}
